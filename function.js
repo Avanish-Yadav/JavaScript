5d0c57b192eff73b61dcb459a5b31aaf215120fb
Avanish-Yadav <avanishyadav1906@gmail.com>
2024-06-01 20:23:42 +0530

    Add function

--- function.js
+++ function.js
@@ -2,9 +2,14 @@ function myFirst() {
     console.log("Hello");
   }
   
-  function mySecond() {
+  function mySecond1() {
     console.log("Goodbye");
+  }
+
+  function mySecond2() {
+    console.log("Goodbye Again");
   }
   
   myFirst();
-  mySecond();
+  mySecond1();
+  mySecond2();
